1db8e2f8585df96321967cd68c9b492bc626e834
Artem Bibaev <bibayev05@bk.ru>
2025-02-13 15:49:00 +0300

    Четвертое тестовое изменение

--- Player_files/player.gd
+++ Player_files/player.gd
@@ -17,8 +17,7 @@ func _physics_process(delta: float) -> void:
 		velocity.y = JUMP_VELOCITY
 		anim.play("Jump")
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
--- Vrag/vrag.gd
+++ Vrag/vrag.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
 
+@onready var anim = $AnimatedSprite2D
+
 var attack: bool = false
 var speed: int = 200
 
@@ -9,20 +11,22 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		
 	var player = $"../Player"
 	var direction = (player.position - self.position).normalized()
 	
 	if attack == true:
 		velocity.x = direction.x * speed
-		print(direction.x)
+		anim.play("Run")
+		if direction.x > 0:
+			$AnimatedSprite2D.flip_h = false
+		else:
+			$AnimatedSprite2D.flip_h = true
 	else:
 		velocity.x = 0 
-	
+		anim.play("Idle")
+		
 	move_and_slide()
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
